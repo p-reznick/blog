b9a8815000b51e5c4adb91e678d89d46d672a262
preznick <preznick@gmail.com>
2017-03-29 01:31:54 +0000

    tweaked scroll to top js and css such that option not available if at top of page

--- public/scripts/to_top.js
+++ public/scripts/to_top.js
@@ -1,13 +1,26 @@
 $(document).ready(function() {
+  scrollToTop();
+});
+
+function scrollToTop() {
   $('h1').on('click', function(e) {
-    $(document).scrollTop(0);
+    if (!atTop()) {
+      $(document).scrollTop(0);
+    }
   });
 
   $('h1').on('mouseenter', function() {
-    $(this).toggleClass('hovered');
+    if (!atTop()) {
+      $(this).addClass('hovered');
+    }
   });
 
   $('h1').on('mouseleave', function() {
-    $(this).toggleClass('hovered');
+      $(this).removeClass('hovered');
   });
-});
+}
+
+function atTop() {
+  console.log('at top');
+  return $(document).scrollTop() == 0;
+}
